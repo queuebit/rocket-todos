728d2b6acbc8689ef4e8ab7a9b2e772065686351
queuebit <queuebit10648@gmail.com>
2015-02-25 15:36:16 -0500

    Makes DB calls secure through use of methods - step 10

--- rocket-todos.js
+++ rocket-todos.js
@@ -21,12 +21,7 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
       console.log(event);
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),
-        owner: Meteor.userId(),
-        username: Meteor.user().username
-      });
+      Meteor.call("addTask", text);
 
       event.target.text.value = "";
 
@@ -38,10 +33,10 @@ if (Meteor.isClient) {
   });
   Template.task.events({
     "click .toggle-checked": function () {
-      Tasks.update(this._id, {$set: {checked: ! this.checked }});
+      Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function () {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
 
@@ -49,4 +44,25 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId)
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  }
+});
 
